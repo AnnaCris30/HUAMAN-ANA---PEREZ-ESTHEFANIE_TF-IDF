8361dfc77c12e564ed14c5911bd9f8a57ec07c61
AnnaCris30 <anacristina.huaman@unmsm.edu.pe>
2023-07-02 13:53:14 -0500

    commit inicial

--- main.c
+++ main.c
@@ -8,10 +8,8 @@ Integrantes:
 */
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
-
-#define MAX_DOCUMENTS 10
+#include <ctype.h>
 #define MAX_WORDS 100
 #define MAX_WORD_LENGTH 50
 
@@ -20,51 +18,51 @@ typedef struct {
     int count;
 } WordInfo;
 
-void calculateTFIDF(float tfidf[MAX_DOCUMENTS][MAX_WORDS], int wordCount[MAX_DOCUMENTS][MAX_WORDS], int numDocuments, int numWords) {
-    int i, j;
-    int docFrequency[MAX_WORDS] = {0};
-
-    // Calcula la frecuencia del documento para cada palabra
-    for (i = 0; i < numDocuments; i++) {
-        for (j = 0; j < numWords; j++) {
-            if (wordCount[i][j] > 0) {
-                docFrequency[j]++;
-            }
-        }
-    }
-
-    // Calcula la puntuación TF-IDF para cada palabra en cada documento
-    for (i = 0; i < numDocuments; i++) {
-        for (j = 0; j < numWords; j++) {
-            float tf = (float) wordCount[i][j] / (float) numWords;
-            float idf = log((float) numDocuments / (float) docFrequency[j]);
-            tfidf[i][j] = tf * idf;
-        }
-    }
-}
 
 int main() {
-    int numDocuments = 3;
-    int numWords = 4;
+    int numWords = 0;
+    WordInfo words[MAX_WORDS] = {0};
 
-    int wordCount[MAX_DOCUMENTS][MAX_WORDS] = {
-        {2, 1, 0, 0},
-        {0, 3, 1, 0},
-        {1, 0, 2, 2}
-    };
+    // Abre el archivo de texto
+    FILE *file = fopen("D:\\test.txt", "r");
+    if (file == NULL) {
+        printf("No se pudo abrir el archivo\n");
+        return 1;
+    }
 
-    float tfidf[MAX_DOCUMENTS][MAX_WORDS];
-
-    calculateTFIDF(tfidf, wordCount, numDocuments, numWords);
-
-    // Imprime la puntuación TF-IDF para cada palabra en cada documento
-    int i, j;
-    for (i = 0; i < numDocuments; i++) {
-        printf("Documento %d:\n", i + 1);
-        for (j = 0; j < numWords; j++) {
-            printf("Palabra %d: %f\n", j + 1, tfidf[i][j]);
+    // Lee el contenido del archivo palabra por palabra
+    char word[MAX_WORD_LENGTH];
+    while (fscanf(file, "%s", word) != EOF) {
+        // Convierte la palabra a minúsculas
+        for (int i = 0; i < strlen(word); i++) {
+            word[i] = tolower(word[i]);
         }
-        printf("\n");
+
+        // Busca la palabra en la lista de palabras
+        int i;
+        for (i = 0; i < numWords; i++) {
+            if (strcmp(words[i].word, word) == 0) {
+                break;
+            }
+        }
+
+        // Si la palabra no está en la lista, agrégala
+        if (i == numWords) {
+            strncpy(words[numWords].word, word, MAX_WORD_LENGTH);
+            words[numWords].count = 1;
+            numWords++;
+        } else {
+            words[i].count++;
+        }
+    }
+
+    // Cierra el archivo
+    fclose(file);
+
+    // Imprime las palabras y sus conteos
+    for (int i = 0; i < numWords; i++) {
+        printf("%s: %d\n", words[i].word, words[i].count);
+	printf("tdif: %d",numWords/words[i].count);
     }
 
     return 0;
